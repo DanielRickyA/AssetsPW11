9ef4a96a0f2ca7d07a4683cf40b32c3db3cbc5b2
DanielRickyA <danielrickya1404@gmail.com>
2023-11-12 22:01:30 +0700

    first commit

---
+++ Image.jsx
@@ -0,0 +1,15 @@
+import poster1 from "./images/poster1.webp";
+import poster2 from "./images/poster2.jpg";
+import poster3 from "./images/poster3.webp";
+import poster4 from "./images/poster4.jpg";
+import poster5 from "./images/poster5.jpg";
+import poster6 from "./images/poster6.png";
+
+export default {
+    Poster1: poster1,
+    Poster2: poster2,
+    Poster3: poster3,
+    Poster4: poster4,
+    Poster5: poster5,
+    Poster6: poster6,
+};
